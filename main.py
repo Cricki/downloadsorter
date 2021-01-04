4e70e53738fdf80c62d7375649a7361fbf2eec88
Cricki <bt1996@gmx.de>
2021-01-04 18:28:20 +0100

    stuff

--- main.py
+++ main.py
@@ -1,9 +1,20 @@
 #!/usr/bin/python3
 import os
+from os import listdir, walk
+from os.path import isfile, join
 
-path = os.path.expanduser('~/Downloads')
+#path = os.path.expanduser('~/Downloads')
+#username = str(os.getlogin()) 
+path = os.path.expanduser('/mnt/c/Users/bt199/Downloads')
+d_folder = [name for name in os.listdir(path)]
 
-b = [name for name in os.listdir(path)]
-print(b)
+files = []
+folders = []
 
+for (dirpath, dirnames, filenames) in walk(path):
+    files.extend(filenames)
+    folders.extend(dirnames)
+    break
 
+print(files)
+print(folders)
